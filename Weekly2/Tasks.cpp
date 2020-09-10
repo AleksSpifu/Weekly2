2afd386fa278d5a5315051b99fce225339917905
Aleksander Furnes <66044119+AleksSpifu@users.noreply.github.com>
2020-09-10 12:38:17 +0200

    Ferdig med hele shaiten

--- Weekly2/Tasks.cpp
+++ Weekly2/Tasks.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 int counter = 0;
+char reply;
 
 void taskOne() {
     counter = 0;
@@ -12,12 +14,16 @@ void taskOne() {
     for (int i = 0; i < 100; i++) {
         std::cout << i + 1 << " ";
     }
+    std::cout << "\n";
+    system("pause");
 }
 
 void taskTwo() {
     for (int i = 0; i < 100; i++) {
         std::cout << 100 - i << " ";
     }
+    std::cout << "\n";
+    system("pause");
 }
 
 void taskThree() {
@@ -26,12 +32,16 @@ void taskThree() {
         std::cout << (counter + 1) * 5 << " ";
         counter++;
     }
+    std::cout << "\n";
+    system("pause");
 }
 
 void taskFour() {
     for (int i = 0; i < 10; i++) {
         std::cout << (i + 1) * 5 << " ";
     }
+    std::cout << "\n";
+    system("pause");
 }
 
 void taskFive() {
@@ -40,15 +50,114 @@ void taskFive() {
         std::cout << (counter + 1) * 5 << " ";
         counter++;
     } while (counter < 10);
+    std::cout << "\n";
+    system("pause");
 }
 
-void taskSixAndSeven() {
-
+void taskSeven() {
+    switch (reply)
+    {
+    case 'a':
+        reply = 'A';
+        break;
+    case 'b':
+        reply = 'B';
+        break;
+    case 'c':
+        reply = 'C';
+        break;
+    case 'd':
+        reply = 'D';
+        break;
+    case 'e':
+        reply = 'E';
+        break;
+    case 'f':
+        reply = 'F';
+        break;
+    }
 }
+
+void taskSix() {
+    std::cout << "What grade would you like to recieve in programming 1? A-F\n";
+    std::cin >>  reply;
+    taskSeven();
+    switch (reply) {
+    case 'A':
+        std::cout << "Outstanding!\n";
+        break;
+    case 'B':
+        std::cout << "Very good \n";
+        break;
+    case 'C':
+        std::cout << "Good\n";
+        break;
+    case 'D':
+        std::cout << "Some flaws\n";
+        break;
+    case 'E':
+        std::cout << "Not very good\n";
+        break;
+    case 'F':
+        std::cout << "Fail\n";
+        break;
+    default:
+        std::cout << "This is not a grade!\n";
+        break;
+    }
+    system("pause");
+}
+
 
 int main()
 {
-    taskFive();
+    std::string waste = "";
+    int menuChoice = 0;
+    bool finished = false;
+    while (finished == false) {
+        system("cls");
+        std::cout << "Which task would you like?\n";
+        std::cout << "1. Task 1\n";
+        std::cout << "2. Task 2\n";
+        std::cout << "3. Task 3\n";
+        std::cout << "4. Task 4\n";
+        std::cout << "5. Task 5\n";
+        std::cout << "6. Task 6 and 7\n";
+        std::cout << "7. Exit\n";
+        std::cin >> menuChoice;
+        std::cin.clear();
+        while (menuChoice < 1 || menuChoice > 7) {
+            std::cin.clear();
+            std::cin.ignore(100, '\n');
+            std::cout << "Please make a valid selection.\n";
+            std::cin >> menuChoice;
+        }
+
+        switch (menuChoice) {
+        case 1:
+            taskOne();
+            break;
+        case 2:
+            taskTwo();
+            break;
+        case 3:
+            taskThree();
+            break;
+        case 4:
+            taskFour();
+            break;
+        case 5:
+            taskFive();
+            break;
+        case 6:
+            taskSix();
+            break;
+        case 7:
+            finished = true;
+            break;
+        }
+    }
+    
     return 0;
 } 
 
